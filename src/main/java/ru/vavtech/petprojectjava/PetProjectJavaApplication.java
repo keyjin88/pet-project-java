755eaa6453f2f2f9ddf6a74be94b9ea0da274dc3
AV <keyjin88@gmail.com>
2025-08-20 22:51:35 +0300

    Initial commit

---
+++ src/main/java/ru/vavtech/petprojectjava/PetProjectJavaApplication.java
@@ -0,0 +1,13 @@
+package ru.vavtech.petprojectjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProjectJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetProjectJavaApplication.class, args);
+    }
+
+}
